85855df2fdb8c3306ce93f46ed2e17006bfbd051
mechanizedpanda <54003040+mechanizedpanda@users.noreply.github.com>
2021-07-30 19:05:42 -0400

    404, css/html changes
    Need to create containers, having overflow issues with website when window size shrinks. Added a 404 page for my own testing.

--- app.py
+++ app.py
@@ -3,9 +3,20 @@ from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/, methods=["GET", "POST"]')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+# def send_sms():
+#     sms_num = request.form.get("phone")
+#     sms_sub = request.form.get("subject")
+#     sms_desc = request.form.get("description")
+#     sms_dt = request.form.get("date-time")
 
 
 if __name__ == "__main__":
