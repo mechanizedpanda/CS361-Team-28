67c3a799d6c5ad2f857f2bd89de43da68f604bd5
mechanizedpanda <54003040+mechanizedpanda@users.noreply.github.com>
2021-08-02 14:55:10 -0400

    app now sends text messages with data from form

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, redirect, url_for, render_template, request
+from twilio.rest import Client
+import time
+import schedule
+from datetime import datetime
 
 app = Flask(__name__)
+
+account = "AC84f950f6bae2ef652bddd8d2200011d9"
+token = "c67cd225bfc1694ac6c52f6fce26b12b"
+client = Client(account, token)
+twilio_number = "18587269285"
+test_msg = "this is a test"
 
 @app.route('/')
 def home():
@@ -8,18 +18,24 @@ def home():
 
 @app.route('/', methods=["GET", "POST"])
 def form():
-    default_name = "0"
-    sms_sub = request.form.get("subject", default_name)
-    sms_desc = request.form.get("description", default_name)
-    sms_date = request.form.get("date", default_name)
-    sms_time = request.form.get("time", default_name)
-    sms_num = request.form.get("phone", default_name)
-    print(sms_sub, sms_desc, sms_date, sms_time, sms_num)
-    return render_template('success.html', sub = sms_sub, desc = sms_desc, date = sms_date, time = sms_time, num = sms_num)
+    sms_desc = request.form.get("description")
+    sms_time = request.form.get("time")
+    sms_num = request.form.get("phone")
+    sms_msg = sms_desc + " " + "today" + " at " + sms_time
+    print(sms_desc, sms_time, sms_num, sms_msg)
+    # schedule.every().day.at(sms_time).do(send_msg(sms_num, sms_desc))
+    client.messages.create(to=sms_num, from_=twilio_number, body=sms_msg)
+    return render_template('success.html', desc=sms_desc, time=sms_time, num=sms_num)
 
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
