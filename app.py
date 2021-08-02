e83d9e9bcf9dcfada6936aea59b789b8d13c600f
mechanizedpanda <54003040+mechanizedpanda@users.noreply.github.com>
2021-08-02 10:57:44 -0400

    add page, grab form data
    added a page for successful form submission, also added functionality to the app to grab the data submitted by the form. successfully grabs form data

--- app.py
+++ app.py
@@ -2,22 +2,24 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route('/, methods=["GET", "POST"]')
+@app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/', methods=["GET", "POST"])
+def form():
+    default_name = "0"
+    sms_sub = request.form.get("subject", default_name)
+    sms_desc = request.form.get("description", default_name)
+    sms_date = request.form.get("date", default_name)
+    sms_time = request.form.get("time", default_name)
+    sms_num = request.form.get("phone", default_name)
+    print(sms_sub, sms_desc, sms_date, sms_time, sms_num)
+    return render_template('success.html')
 
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html")
 
-# def send_sms():
-#     sms_num = request.form.get("phone")
-#     sms_sub = request.form.get("subject")
-#     sms_desc = request.form.get("description")
-#     sms_dt = request.form.get("date-time")
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
